ed0c2fa3716ebd8828c12da02c35b429866b1ded
Silver <borde.rajat@gmail.com>
2021-12-12 13:19:03 +0530

    Commented removed unsed variables and imports

--- src/main/java/com/postgres/controller/CityController.java
+++ src/main/java/com/postgres/controller/CityController.java
@@ -10,13 +10,9 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -29,7 +25,7 @@ import com.postgres.service.impl.CityServiceImpl;
 public class CityController {
 	
 	private static final Logger LOGGER = LogManager.getLogger(CityServiceImpl.class.getName());
-	private static final String EXCEPTION_MSG = "Exception occured";
+	//private static final String EXCEPTION_MSG = "Exception occured";
 
 	@Autowired
 	private CityService cityService;
--- src/main/java/com/postgres/service/impl/CityServiceImpl.java
+++ src/main/java/com/postgres/service/impl/CityServiceImpl.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +17,8 @@ import com.postgres.service.CityService;
 @Service
 public class CityServiceImpl implements CityService {
 
-	private static final Logger LOGGER = LogManager.getLogger(CityServiceImpl.class.getName());
-	private static final String EXCEPTION_MSG = "Exception occured";
+	//private static final Logger LOGGER = LogManager.getLogger(CityServiceImpl.class.getName());
+	//private static final String EXCEPTION_MSG = "Exception occured";
 
 	@Autowired
 	private CityDao cityDao;
